671005f423c80085eecac75a1d8a61eda17da410
GustafWallstrom <46966247+GustafWallstrom@users.noreply.github.com>
2020-09-29 14:59:14 +0200

    more changes

--- Lab1/Lab1-part1.py
+++ Lab1/Lab1-part1.py
@@ -1,4 +1,3 @@
-print(sys.version_info)
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
@@ -8,7 +7,66 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 import os
-print("")
-for dirname, _, filenames in os.walk('/data'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+
+
+#Load datasets to create two dataframes
+
+train_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
+
+train = pd.read_csv(train_url)
+
+test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
+
+test = pd.read_csv(test_url)
+
+
+# for dirname, _, filenames in os.walk('/data'):
+#     for filename in filenames:
+#         print(os.path.join(dirname, filename))
+
+
+train = train.drop(['Name','Ticket','Cabin','Embarked'],axis = 1)
+
+test = test.drop(['Name','Ticket','Cabin','Embarked'],axis = 1)
+
+test.fillna(train.mean(),inplace = True)
+train.fillna(train.mean(),inplace = True)
+
+labelEncoder = LabelEncoder()
+labelEncoder.fit(train['Sex'])
+labelEncoder.fit(test['Sex'])
+train['Sex'] = labelEncoder.transform(train['Sex'])
+test['Sex'] = labelEncoder.transform(test['Sex'])
+
+X = np.array(train.drop(['Survived'], 1).astype(float))
+y = np.array(train['Survived'])
+
+scaler = MinMaxScaler()
+X_scaled = scaler.fit_transform(X)
+
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(X_scaled)
+KMeans(algorithm='auto', copy_x=True, init='k-means++', max_iter=300,
+    n_clusters=2, n_init=10, n_jobs=1, precompute_distances='auto',
+    random_state=None, tol=0.0001, verbose=0)
+
+correct = 0
+for i in range(len(X)):
+    predict_me = np.array(X[i].astype(float))
+    predict_me = predict_me.reshape(-1, len(predict_me))
+    prediction = kmeans.predict(predict_me)
+    if prediction[0] == y[i]:
+        correct += 1
+print(correct/len(X))
+
+# print("***** Train_Set *****")
+# print(train.isna().sum())
+# print("\n")
+# print("***** Test_Set *****")
+# print(test.isna().sum())
+
+# g = sns.FacetGrid(train, col = 'Survived')
+# g.map(plt.hist, 'Age', bins = 20)
+# plt.show()
+
+# <seaborn.axisgrid.FacetGrid at 0x7fa990f87080>
